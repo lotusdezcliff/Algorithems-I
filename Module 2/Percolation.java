7ff37daa5fa501f946d8293627b3ba8faae0b656
Aaron LOI <boddy.jo.bj@gmail.com>
2025-08-02 18:05:38 +0800

    working on Percolation.java

--- Module 2/Percolation.java
+++ Module 2/Percolation.java
@@ -1,23 +1,42 @@
 public class Percolation {
-
+    private final boolean[][] grid;
     // creates n-by-n grid, with all sites initially blocked
-    public Percolation(int n)
+    public Percolation(int n) {
+        grid = new boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = false;
+            }
+        }
+    }
 
     // opens the site (row, col) if it is not open already
-    public void open(int row, int col)
+    public void open(int row, int col) {
+        grid[row][col] = true;
+    }
 
     // is the site (row, col) open?
-    public boolean isOpen(int row, int col)
+    public boolean isOpen(int row, int col) {
+        return grid[row][col];
+    }
 
     // is the site (row, col) full?
-    public boolean isFull(int row, int col)
+    public boolean isFull(int row, int col) {
+        
+    }
 
     // returns the number of open sites
-    public int numberOfOpenSites()
+    public int numberOfOpenSites() {
+
+    }
 
     // does the system percolate?
-    public boolean percolates()
+    public boolean percolates() {
+
+    }
 
     // test client (optional)
-    public static void main(String[] args)
+    public static void main(String[] args) {
+
+    }
 }
